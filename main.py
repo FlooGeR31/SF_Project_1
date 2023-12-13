fa3529f679a1dc4f5e3b60185189b73b7d5492f6
Denis S <denis13th@gmail.com>
2023-12-13 18:02:41 +0300

    Streamlit and OpenCV now are friends

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import cv2
+import io
+import streamlit as st
+from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
+
+cascade_path = "filters/cascade.xml"
+video_path = "data/video1.mp4"
 
 def face_capture():
+    st.set_page_config(page_title="Streamlit + OpenCV приложение")
+    st.title("Steamlit приложение")
+    st.caption("OpenCV, Streamlit")
 
-    cascade_path = r"C:\Users\Alex\Desktop\Education\SF\Project 1\filters"
+    frame_placeholder = st.empty()
+    stop_button_pressed = st.button("Stop")
 
-    video_path = r"C:\Users\Alex\Desktop\Education\SF\Project\SF_Project_1sem\videos\video1.mov"
-
-    
     # Создание объекта классификатора для обнаружения лиц
     clt = cv2.CascadeClassifier(cascade_path)
 
@@ -20,25 +31,26 @@ def face_capture():
         return
 
     # Бесконечный цикл для обработки каждого кадра видео
-    while True:
+    while True and not stop_button_pressed:
         # Чтение кадра из видеофайла
         ret, frame = camera.read()
 
         # Проверка наличия кадра и успешности его чтения
         if not ret:
+            st.write("Видео закончилось.")
             print("Ошибка: Не удалось прочитать кадр.")
             break
 
         # Преобразование цветного изображения в оттенки серого
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Обнаружение лиц на кадре
         faces = clt.detectMultiScale(
-            gray,
-            scaleFactor=1.02,
+            frame,
+            scaleFactor=1.22,
             minNeighbors=7,
-            minSize=(160, 160),
-            maxSize=(230,230)
+            minSize=(80, 80),
+            maxSize=(240, 240)
         )
 
         # Отрисовка прямоугольников вокруг обнаруженных лиц
@@ -46,7 +58,8 @@ def face_capture():
             cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 2)
 
         # Отображение обработанного кадра с лицами
-        cv2.imshow("Лица", frame)
+        frame_placeholder.image(frame, channels='BGR')
+        # cv2.imshow("Лица", frame)
 
         # Выход из программы при нажатии клавиши 'q'
         key = cv2.waitKey(1)
@@ -61,8 +74,5 @@ def face_capture():
 def main():
     face_capture()
 
-
 if __name__ == "__main__":
     main()
-
-
